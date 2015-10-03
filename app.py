5eb649e8747b22d9eb2bcf8501003a3c5e9ec1fd
Iain Harlow <iainharlow@yahoo.co.uk>
2015-10-03 13:15:24 -0700

    m

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect
-import Quandl
 import pandas as pd
 import numpy as np
 from bokeh.embed import components
@@ -9,42 +8,56 @@ from bokeh.templates import RESOURCES
 from bokeh.util.string import encode_utf8
 from bokeh.charts import TimeSeries
 
-app = Flask(__name__)
+app_polinet = Flask(__name__)
 
-app.vars = {}
+app_polinet.vars = {}
 
+'''
+Main functions here
+'''
 
-def stock_to_caps(stock_name):
+def get_pol_name(pol_name):
 	'''
-	Simple test function. stock_name should be a string, this converts it
-	to caps and chops off all but the first four letters.
+	Simple test function. pol_name should be a string, this converts it
+	to caps.
 	'''
-	return stock_name.upper()[:4]
+	return pol_name.upper()
 
-def get_data(stock_name):
-	return Quandl.get("".join(["WIKI/",stock_to_caps(stock_name),".4"]),authtoken="ppGMYxKEUmJ16a11sa23")
+def get_pol_data(pol_name):
+	'''
+	Look up the basic details for the politician chosen.
+	'''
+	return pol_name.upper()
 
 
 
-@app.route('/')
-def redirect_to_index():
-	return redirect('/index')
 
-@app.route('/index', methods=['POST','GET'])
-def index():
+
+'''
+Route functions here
+'''
+
+@app_polinet.route('/')
+def redirect_to_polinet():
+	return redirect('/polinet')
+
+
+@app_polinet.route('/polinet', methods=['POST','GET'])
+def polinet():
+
 	error = None
 	if request.method == 'POST':
 		try:
-			app.vars['stock_name'] = stock_to_caps(request.form['ticker'])
-			app.vars['data_table'] = get_data(app.vars['stock_name'])
+			app_polinet.vars['pol_name'] = get_pol_name(request.form['pol_name'])
 		except:
-			app.vars['stock_name'] = 'GOOG'
-			app.vars['data_table'] = get_data('GOOG')
+			app_polinet.vars['pol_name'] = 'Not a valid name'
 
 	else:
-		app.vars['stock_name'] = 'GOOG'
-		app.vars['data_table'] = get_data('GOOG')
-	fig = TimeSeries(app.vars['data_table'])
+		app_polinet.vars['pol_name'] = 'Bernard Sanders'
+
+
+	app.vars['pol_data'] = pd.DataFrame([[1,2,3,1,3,2,4,1]])
+	fig = TimeSeries(app.vars['pol_data'])
 
 	plot_resources = RESOURCES.render(
 		js_raw=INLINE.js_raw,
@@ -54,15 +67,19 @@ def index():
 		)
 	script, div = components(fig, INLINE)
 
-	return render_template('index.html',
-		                   name=app.vars['stock_name'],
-		                   data=app.vars['data_table'],
+	return render_template('polinet.html',
+		                   name=app_polinet.vars['pol_name'],
+		                   data=app_polinet.vars['pol_data'],
 		                   plot_script=script,
 		                   plot_div=div,
 		                   plot_resources=plot_resources)
-	
+
+'''
+Run app
+'''
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
+    app_polinet.run(host='0.0.0.0', debug=False)
 
 
 
