75e16b643c43a3b12e5e122d78904d1b754a0c81
Iain Harlow <iainharlow@yahoo.co.uk>
2015-10-03 13:52:21 -0700

    message

--- app.py
+++ app.py
@@ -45,41 +45,41 @@ def redirect_to_polinet():
 @app_polinet.route('/polinet', methods=['POST','GET'])
 def polinet():
 
-	error = None
-	if request.method == 'POST':
-		try:
-			app_polinet.vars['pol_name'] = get_pol_name(request.form['pol_name'])
-		except:
-			app_polinet.vars['pol_name'] = 'Not a valid name'
+	#error = None
+	#if request.method == 'POST':
+	#	try:
+	#		app_polinet.vars['pol_name'] = get_pol_name(request.form['pol_name'])
+	#	except:
+	#		app_polinet.vars['pol_name'] = 'Not a valid name'
 
-	else:
-		app_polinet.vars['pol_name'] = 'Bernard Sanders'
+	#else:
+	#	app_polinet.vars['pol_name'] = 'Bernard Sanders'
 
+    app_polinet.vars['pol_name'] = 'Bernard Sanders'
+	app_polinet.vars['pol_data'] = [1,2] #pd.DataFrame([1,2,3,1,3,2,4,1])
+	#fig = TimeSeries(app.vars['pol_data'])
 
-	app.vars['pol_data'] = pd.DataFrame([[1,2,3,1,3,2,4,1]])
-	fig = TimeSeries(app.vars['pol_data'])
-
-	plot_resources = RESOURCES.render(
-		js_raw=INLINE.js_raw,
-		css_raw=INLINE.css_raw,
-		js_files=INLINE.js_files,
-		css_files=INLINE.css_files
-		)
-	script, div = components(fig, INLINE)
+	#plot_resources = RESOURCES.render(
+	#	js_raw=INLINE.js_raw,
+	#	css_raw=INLINE.css_raw,
+	#	js_files=INLINE.js_files,
+	#	css_files=INLINE.css_files
+	#	)
+	#script, div = components(fig, INLINE)
 
 	return render_template('polinet.html',
 		                   name=app_polinet.vars['pol_name'],
-		                   data=app_polinet.vars['pol_data'],
-		                   plot_script=script,
-		                   plot_div=div,
-		                   plot_resources=plot_resources)
+		                   data=app_polinet.vars['pol_data'])#,
+		                   #plot_script=script,
+		                   #plot_div=div,
+		                   #plot_resources=plot_resources)
 
 '''
 Run app
 '''
 
 if __name__ == '__main__':
-    app_polinet.run(host='0.0.0.0', debug=False)
+    app_polinet.run()#host='0.0.0.0', debug=False)
 
 
 
